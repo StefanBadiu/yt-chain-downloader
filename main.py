46e465a3d4217063de4d69875e2d2d352faff462
Stefan Badiu <sbadiu@purdue.edu>
2025-07-22 12:22:28 -0400

    main code

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import filedialog
+from yt_dlp import YoutubeDL
+
+def choose_download_path():
+    root = tk.Tk()
+    root.withdraw()  #hide GUI window
+    folder_selected = filedialog.askdirectory(title="Select download folder")
+    return folder_selected
+
+def download_audio_from_playlist(playlist_url, download_path):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'extract_audio': True,
+        'audio_format': 'mp3',
+        'outtmpl': f'{download_path}/%(title)s.%(ext)s'
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([playlist_url])
+
+# Example usage
+playlist_url = input("Paste YouTube playlist URL: ")
+download_path = choose_download_path()
+download_audio_from_playlist(playlist_url, download_path)
